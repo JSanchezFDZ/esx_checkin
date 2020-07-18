5a6dc9ce778203e95d3b29acfb235809763990af
AlwaysCurly <52683582+AlwaysCurly@users.noreply.github.com>
2020-07-17 23:02:11 -0500

    v1.1 updates
    no more press key to leave bed and new config option

--- config.lua
+++ config.lua
@@ -5,6 +5,9 @@ Config.Locale = 'en'
 -- Distance before text is shown
 Config.TextDrawDistance = 5  
 
+-- Time in seconds before player gets out of bed 
+Config.BedTime = 10
+
 Config.Locations = {
 	PillBoxHospital = {
 		Coords = {x = 305.996, y = -596.543, z = 44.5}
